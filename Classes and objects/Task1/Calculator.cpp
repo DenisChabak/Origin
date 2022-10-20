945c1cd5ac9298b196ae8e30fa012e5c2bdc1592
Denis Chabak <dchabak@yandex.ru>
2022-10-20 20:37:26 +0300

    ClassandObjects

---
+++ Classes and objects/Task1/Calculator.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <string>
+
+class Calculator
+{
+public:
+	float Num1;
+	float Num2;
+
+		double add(float Num1, float Num2)
+		{
+			std::cout << "Num1 + Num2 = " << Num1 + Num2 << std::endl;
+			return Num1 + Num2;
+		}
+
+		double multiply(float Num1, float Num2)
+		{
+			std::cout << "Num1 * Num2 = " << Num1 * Num2 << std::endl;
+			return Num1 * Num2;
+		}
+
+		double subtract_1_2(float Num1, float Num2)
+		{
+			std::cout << "Num1 - Num2 = " << Num1 - Num2 << std::endl;
+			return Num1 - Num2;
+		}
+
+		double subtract_2_1(float Num1, float Num2)
+		{
+			std::cout << "Num2 - Num1 = " << Num2 - Num1 << std::endl;
+			return Num2 - Num1;
+		}
+
+		double divide_1_2(float Num1, float Num2)
+		{
+			std::cout << "Num1 / Num2 = " << Num1 / Num2 << std::endl;
+			return Num1 / Num2;
+		}
+
+		double divide_2_1(float Num1, float Num2)
+		{
+			std::cout << "Num2 / Num1 = " << Num2 / Num1 << std::endl;
+			return Num2 / Num1;
+		}
+
+		bool set_num1(double Num1)
+		{
+			this->Num1 = Num1;
+			if (Num1 == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1;
+			}
+			
+		}
+
+		bool set_num2(double Num2)
+		{
+			this->Num2 = Num2;
+			if (Num2 == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1;
+			}
+
+		}
+	
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	while (true)
+	{
+
+
+		float Num1, Num2;
+		Calculator Num;
+
+		Input:
+		std::cout << "Введите Num1: ";
+		std::cin >> Num1;
+
+		if (!Num.set_num1(Num1))
+		{
+			std::cout << "Неверный ввод!" << std::endl;
+			goto Input;
+		}
+
+		Input2:
+		std::cout << "Введите Num2: ";
+		std::cin >> Num2;
+
+		if (!Num.set_num2(Num2))
+		{
+			std::cout << "Неверный ввод!" << std::endl;
+			goto Input2;
+		}
+					
+		
+		Num.add(Num1, Num2);
+		Num.multiply(Num1, Num2);
+		Num.subtract_1_2(Num1, Num2);
+		Num.subtract_2_1(Num1, Num2);
+		Num.divide_1_2(Num1, Num2);
+		Num.divide_2_1(Num1, Num2);
+		
+
+	}
+}
+
---
+++ Classes and objects/Task2/Counter.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream> 
+#include <stdexcept>
+
+
+
+class Counter
+{
+private:
+	int count = 1;
+
+public:
+
+	int get_count()
+	{
+		count = 1;
+		return count;
+	}
+
+	int set_count(int number)
+	{
+		this->count = number;
+		return count;
+	}
+	int adder()
+	{
+		count++;
+		return count;
+	}
+
+	int subtracter()
+	{
+		count--;
+		return count;
+	}
+
+	void show()
+	{
+		std::cout << count << std::endl;
+	}
+
+	void exit()
+	{
+		std::cout << "До свидания!" << std::endl;
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	Counter num;
+	int number;
+	char command;
+	std::string answer;
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите 'yes'' или 'no': ";
+	std::cin >> answer;
+	if (answer == "yes")
+	{
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> number;
+		num.set_count(number);
+	}
+	else
+	{
+		num.get_count();
+	}
+
+	while (true)
+	{
+
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> command;
+		if (command == '+')
+		{
+			num.adder();
+		}
+		else if (command == '-')
+		{
+			num.subtracter();
+		}
+		else if (command == '=')
+		{
+			num.show();
+		}
+		else if (command == 'x')
+		{
+			num.exit();
+			break;
+		}
+		else
+		{
+			std::cout << "Пожалуйста, вводите только '+', '-', '=' или 'x'" << std::endl;
+		}
+	}
+
+
+	return 0;
+}
+
